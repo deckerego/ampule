572164d5b40b9710034c47b5fd1c119a4aed203c
John Ellis <john@deckerego.net>
2021-09-16 17:45:48 -0400

    Fixed issue with socket handling on inbound requests (#5)

--- tests/test_codes.py
+++ tests/test_codes.py
@@ -9,10 +9,3 @@ def test_socket_error():
     ampule.listen(socket)
     socket.send.assert_called_once_with(http_helper.expected_response(500, "Error processing request"))
     socket.close.assert_called_once()
-
-def test_empty_request():
-    socket = mocket.Mocket("GET /bad HTTP/1.1".encode("utf-8"))
-    socket.recv_into = mock.Mock(side_effect=OSError('ReceiveInto'))
-    ampule.listen(socket)
-    socket.send.assert_called_once_with(http_helper.expected_response(204, ""))
-    socket.close.assert_called_once()
