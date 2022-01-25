06fc41d24ffa2a08445aa4e208bd114c9b5d779d
John Ellis <532789+deckerego@users.noreply.github.com>
2022-01-25 17:50:46 -0500

    Clear up the resend test case a bit

--- tests/test_body.py
+++ tests/test_body.py
@@ -41,6 +41,9 @@ def test_body_resend():
     body = "Let me tell you a long rambling story child. Once upon a time there was a coaster on my desk that had a picture of a ship. It was blue. Blue like the ocean? No. Blue like a coaster."
     socket = mocket.Mocket(("GET /var/get HTTP/1.1\r\n\r\n" + body).encode("utf-8"))
     ampule.listen(socket)
-    expected = [mock.call(http_helper.expected_response(200, "GET: "+body)), mock.call(b"e ocean? No. Blue like a coaster.\r\n")]
+    expected = [ # Expect two calls, the second after an 256 byte partial data send
+        mock.call(http_helper.expected_response(200, "GET: "+body)),
+        mock.call(b"e ocean? No. Blue like a coaster.\r\n")
+    ]
     assert socket.send.mock_calls == expected
     socket.close.assert_called_once()
